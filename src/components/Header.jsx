1d343610b5585795f61c82a9b289b2634287322c
Therese <thereseskold@live.com>
2023-10-17 16:41:13 +0200

    made the setup for the projec

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+
+	return (
+		<header>
+			<img src='./src/assets/imgs/logo.png'></img>
+			<h1 className="header-head"> MOVIESTATS </h1>
+			<p className="header-underline"> - A statistic adventure from <span>MovieFlex</span></p>
+		</header>
+	)
+		
+}
+
+export default Header
---
+++ src/components/StartPage.jsx
@@ -0,0 +1,13 @@
+const Startpage = () => {
+
+	return (
+		<div className="start-container">
+			<button className="start-btn">It all starts with one click</button>
+		</div>
+	)
+
+}
+
+	
+
+export default Startpage
